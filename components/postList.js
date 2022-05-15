3f2b315a39c13fe71fce8b6a45b402c4aff9326d
pulcherriman <tsukasawa_agu@yahoo.co.jp>
2022-05-15 20:56:27 +0900

    fix time

--- components/postList.js
+++ components/postList.js
@@ -55,10 +55,26 @@ export default function PostList() {
 	};
 
 	const dateStringToRelative = (date) => {
-		const dateDiff = (new Date(date)).getDate() - (new Date()).getDate();
-		return new Intl.RelativeTimeFormat('ja', {
-			numeric: 'auto',
-		}).format(dateDiff, "day");
+		const dateObj = new Date(date);
+		const dateDiff = parseInt((dateObj - Date.now()) / 1000 / 60 / 60 / 24);
+		console.log(dateDiff);
+		if (-dateDiff <= 30) {
+			const rtf = new Intl.RelativeTimeFormat('ja', {
+				numeric: 'auto',
+			});
+			return rtf.format(dateDiff, "day");
+		}
+
+		const formatted = `
+			${dateObj.getMonth()+1}月
+			${dateObj.getDate()}日 
+			${dateObj.getHours()}:
+			${dateObj.getSeconds()}
+			`.replace(/\n|\r|\t/g, '');
+		if (dateObj.getFullYear() !== (new Date()).getFullYear()) {
+			return `${dateObj.getFullYear()}年${formatted}`;
+		}
+		return formatted;
 	};
 
 	const renderItem = ({ item }) => (
